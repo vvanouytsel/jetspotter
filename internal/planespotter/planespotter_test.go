722396dac51be058e8520f2431ce9eed02eb5c2b
Vincent Van Ouytsel <vvanouytsel@gmail.com>
2025-01-13 16:44:55 +0100

    test: use correct URL

--- internal/planespotter/planespotter_test.go
+++ internal/planespotter/planespotter_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestGetImageFromAPI(t *testing.T) {
 	// 44c1e5
 	expectedThumbnailURL := "https://t.plnspttrs.net/21151/1449607_d9b2824f6f_280.jpg"
-	expectedImageURL := "https://www.planespotters.net/photo/1449607/g-10-belgium-politiepolice-mcdonnell-douglas-md-900-explorer?utm_source=api"
+	expectedImageURL := "https://www.planespotters.net/photo/1449607/g-10-belgium-politie-police-mcdonnell-douglas-md-900-explorer?utm_source=api"
 	image := GetImageFromAPI("44c1e5", "notimportant")
 
 	if expectedThumbnailURL != image.ThumbnailLarge.Src {
