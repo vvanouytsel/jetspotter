30406561f73ed9183733e0f74e3b3d04277d9346
Vincent Van Ouytsel <vvanouytsel@gmail.com>
2025-05-03 23:37:36 +0200

    Ntfy improvements (#50)
    * Add Actions for Ntfy notifications
    
    Tracker URL now shows up as an action button. For reference: https://docs.ntfy.sh/publish/#action-buttons
    
    * feat: send single notifcation per aircraft
    
    Previously all spotted aircraft were grouped together. If too many
    aircraft were spotted at once ntfy.sh would return an error because the
    payload was too big.
    
    Furthermore the added feature to enable a track 'action' button was not
    usable since it only showed a single button for the last aircraft that
    was spotted.
    
    Now a ntfy message (with track button) is send for each spotted
    aircraft.
    
    ---------
    
    Co-authored-by: Nebula <40148908+nebula-it@users.noreply.github.com>

--- internal/notification/ntfy.go
+++ internal/notification/ntfy.go
@@ -6,33 +6,56 @@ import (
 	"jetspotter/internal/jetspotter"
 )
 
-type NtfyNotification struct {
-	Message  string   `json:"message,omitempty"`
-	Topic    string   `json:"topic,omitempty"`
-	Title    string   `json:"title,omitempty"`
-	Markdown bool     `json:"markdown,omitempty"`
-	Tags     []string `json:"tags,omitempty"`
+type NtfyAction struct {
+	Action string `json:"action"`
+	Clear  bool   `json:"clear,omitempty"`
+	Label  string `json:"label"`
+	URL    string `json:"url"`
 }
 
-// SendNtfyMessage sends a ntfy message containing metadata of a list of aircraft
+type NtfyNotification struct {
+	Actions  []NtfyAction `json:"actions,omitempty"`
+	Message  string       `json:"message,omitempty"`
+	Topic    string       `json:"topic,omitempty"`
+	Title    string       `json:"title,omitempty"`
+	Markdown bool         `json:"markdown,omitempty"`
+	Tags     []string     `json:"tags,omitempty"`
+}
+
+// SendNtfyMessage sends a ntfy message containing metadata of aircraft
+// Each aircraft will have its own separate notification
 func SendNtfyMessage(aircraft []jetspotter.AircraftOutput, config configuration.Config) error {
-	message, err := buildNtfyMessage(aircraft, config)
-	if err != nil {
-		return err
-	}
+	// Send a separate message for each aircraft
+	for _, ac := range aircraft {
+		// Build a message for a single aircraft
+		singleAircraftMessage, err := buildNtfyMessage([]jetspotter.AircraftOutput{ac}, config)
+		if (err != nil) {
+			return err
+		}
 
-	notification := Notification{
-		Message: message,
-		Type:    Ntfy,
-		URL:     config.NtfyServer,
-	}
+		notification := Notification{
+			Message: singleAircraftMessage,
+			Type:    Ntfy,
+			URL:     config.NtfyServer,
+		}
 
-	err = SendMessage(aircraft, notification)
-	if err != nil {
-		return err
+		err = SendMessage([]jetspotter.AircraftOutput{ac}, notification)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
+}
+
+// Constructor function for NtfyAction thats sets default value for Action and Clear
+func AddNtfyAction(label, url string) NtfyAction {
+	return NtfyAction{
+		Action: "view",
+		Clear:  true,
+		Label:  label,
+		URL:    url,
+	}
 }
 
 func buildNtfyMessage(aircraft []jetspotter.AircraftOutput, config configuration.Config) (message NtfyNotification, err error) {
@@ -42,24 +65,26 @@ func buildNtfyMessage(aircraft []jetspotter.AircraftOutput, config configuration
 	message.Markdown = true
 
 	for _, ac := range aircraft {
-		message.Message += "\\=\\=\\=\\=\\=\\=\\=\\=\\=\\=\\=\\=\\=\\=\\=\\=\\=\\=\n"
-		message.Message += fmt.Sprintf("Callsign: %s\n", formatCallsign(ac, Markdown))
-		message.Message += fmt.Sprintf("Registration: %s\n", formatRegistration(ac, Markdown))
-		message.Message += fmt.Sprintf("Country: %s\n", ac.Country)
-		message.Message += fmt.Sprintf("Speed: %s\n", printSpeed(ac))
-		message.Message += fmt.Sprintf("Altitude: %s\n", printAltitude(ac))
-		message.Message += fmt.Sprintf("Distance: %s\n", printDistance(ac))
-		message.Message += fmt.Sprintf("Bearing from location: %s\n", printBearingFromLocation(ac))
-		message.Message += fmt.Sprintf("Bearing to location: %s\n", printBearingFromAircraft(ac))
-		message.Message += fmt.Sprintf("Heading: %s\n", printHeading(ac))
-		message.Message += fmt.Sprintf("Cloud coverage: %s\n", printCloudCoverage(ac))
-		message.Message += fmt.Sprintf("Inbound: %s\n", getInboundStatus(ac))
-		message.Message += fmt.Sprintf("Type: %s\n", ac.Type)
-		message.Message += fmt.Sprintf("Origin: %s\n", printOriginName(ac))
-		message.Message += fmt.Sprintf("Destination: %s\n", printDestinationName(ac))
-		message.Message += fmt.Sprintf("Airline: %s\n", printAirlineName(ac))
-		message.Message += fmt.Sprintf("TrackerURL: %s\n", ac.TrackerURL)
-		message.Message += fmt.Sprintf("ImageURL: %s\n", ac.ImageURL)
+		message.Message += fmt.Sprintf("Callsign:               %s\n", formatCallsign(ac, Markdown))
+		message.Message += fmt.Sprintf("Registration:           %s\n", formatRegistration(ac, Markdown))
+		message.Message += fmt.Sprintf("Country:                %s\n", ac.Country)
+		message.Message += fmt.Sprintf("Speed:                  %s\n", printSpeed(ac))
+		message.Message += fmt.Sprintf("Altitude:               %s\n", printAltitude(ac))
+		message.Message += fmt.Sprintf("Distance:               %s\n", printDistance(ac))
+		message.Message += fmt.Sprintf("Bearing from location:  %s\n", printBearingFromLocation(ac))
+		message.Message += fmt.Sprintf("Bearing to location:    %s\n", printBearingFromAircraft(ac))
+		message.Message += fmt.Sprintf("Heading:                %s\n", printHeading(ac))
+		message.Message += fmt.Sprintf("Cloud coverage:         %s\n", printCloudCoverage(ac))
+		message.Message += fmt.Sprintf("Inbound:                %s\n", getInboundStatus(ac))
+		message.Message += fmt.Sprintf("Type:                   %s\n", ac.Type)
+		message.Message += fmt.Sprintf("Origin:                 %s\n", printOriginName(ac))
+		message.Message += fmt.Sprintf("Destination:            %s\n", printDestinationName(ac))
+		message.Message += fmt.Sprintf("Airline:                %s\n", printAirlineName(ac))
+		message.Message += fmt.Sprintf("ImageURL:               %s\n", ac.ImageURL)
+		// Add Ntfy Actions
+		message.Actions = []NtfyAction{
+			AddNtfyAction("Track Aircraft", ac.TrackerURL),
+		}
 	}
 
 	return message, nil
